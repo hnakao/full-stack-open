4e33a81bcff597b1ade2beca6aee1d5f1ce48b46
Hiroshi Nakao <irochi@onlive.site>
2024-11-10 21:20:21 -0500

    Implement a search field by name

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,8 +2,12 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "123" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
+
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
 
@@ -29,12 +33,31 @@ const App = () => {
     setNewNumber(e.target.value);
   };
 
+  const handleSearch = (e) => {
+    const search = e.target.value;
+    const filteredPersons = persons.filter((person) =>
+      person.name.toLowerCase().includes(search.toLowerCase())
+    );
+    setPersons(filteredPersons);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        <label htmlFor="search">Filter shown with: </label>
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Search by name"
+          onChange={handleSearch}
+        />
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={onSubmit}>
         <div>
-          name:{" "}
+          <label htmlFor="name">Name: </label>
           <input
             type="text"
             name="name"
@@ -43,7 +66,7 @@ const App = () => {
           />
         </div>
         <div>
-          number:{" "}
+          <label htmlFor="phone">Phone: </label>
           <input
             type="text"
             name="phone"
