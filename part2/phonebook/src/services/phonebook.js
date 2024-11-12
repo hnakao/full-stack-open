ba8611967bedcf13c0841d78894643783ae480fa
Hiroshi Nakao <irochi@onlive.site>
2024-11-11 23:02:23 -0500

    Create phonebook service

---
+++ part2/phonebook/src/services/phonebook.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const getAllPersons = async () => {
+  const response = await axios.get("http://localhost:3001/persons");
+  return response.data;
+};
+
+const addPerson = async (newPerson) => {
+  const response = await axios.post("http://localhost:3001/persons", newPerson);
+  return response.data;
+};
+
+const updatePerson = async (person) => {
+  const response = await axios.put(
+    `http://localhost:3001/persons/${person.id}`,
+    person
+  );
+  return response.data;
+};
+
+export { getAllPersons, addPerson, updatePerson };
